32e789ee2568fd80f2cda30251e77b5fc5005f3a
Thanwarat142158 <eeytc8@nottingham.ac.uk>
2022-04-05 15:23:41 +0100

    Worksheet 8

---
+++ Worksheet_8/maths.h
@@ -0,0 +1,12 @@
+/** @file
+* This file contains the declarations of all exported functions.
+*/
+#ifndef MATHS_H
+#define MATHS_H
+
+/** Brief description
+* This description will hopefully be picked up on by doxygen
+*/
+int add( int a, int b );
+#endif
+// /maths.h -------------------------------------
